0d09e958ede7be3b4521ed726d7924ca99669d19
JacobMason83 <jmason062083@gmail.com>
2020-12-15 09:20:48 -0500

    added vanilla javascript color changing background buton to homework file

---
+++ homework/Projects/randomhexNav/script.js
@@ -0,0 +1,33 @@
+// import variables from './variables.scss'
+
+
+
+
+const changeColor = document.getElementById('btnColor')
+const navBar = document.getElementById('navBar')
+const hexValue = document.querySelector('.hexColor')
+if(changeColor) {
+changeColor.addEventListener('click', () => {
+    let color = '#'
+    color += Math.random().toString(16).slice(2, 8)
+    
+
+
+    navBar.style.backgroundColor = color
+    hexValue.innerText = `Poof its magic your color is now: ${color}`
+})}
+
+if (changeColor){
+    changeColor.addEventListener('mouseover', () => {
+        let color = '#'
+        color += Math.random().toString(16).slice(2,8)
+
+        changeColor.style.background = color
+        changeColor.innerText = `Magic is everywhere lets test it`
+        hexValue.innerText = `Alakazam.... `
+    })
+}
+
+
+
+
